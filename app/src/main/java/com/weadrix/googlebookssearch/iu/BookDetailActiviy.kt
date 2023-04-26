e62847d96b3b79ebe407bf7a3847ae71606a1208
Wadrian Araujo <wadm.snf20@uea.edu.br>
2023-04-26 19:05:51 -0400

    add : added coverImage to list books
    added images to each book from rest api
    added image in case imageCover comes null

---
+++ app/src/main/java/com/weadrix/googlebookssearch/iu/BookDetailActiviy.kt
@@ -0,0 +1,12 @@
+package com.weadrix.googlebookssearch.iu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.weadrix.googlebookssearch.R
+
+class BookDetailActiviy : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_book_detail)
+    }
+}
--- app/src/main/java/com/weadrix/googlebookssearch/iu/adapter/BookListAdapter.kt
+++ app/src/main/java/com/weadrix/googlebookssearch/iu/adapter/BookListAdapter.kt
@@ -3,6 +3,7 @@ package com.weadrix.googlebookssearch.iu.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 import com.weadrix.googlebookssearch.R
 import com.weadrix.googlebookssearch.databinding.ItemBookBinding
 import com.weadrix.googlebookssearch.model.Volume
@@ -21,6 +22,11 @@ class BookListAdapter(
 
     override fun onBindViewHolder(holder: BookHolder, position: Int) {
         val volume = items[position]
+        if (volume.volumeInfo.imageLinks?.smallThumbnail != null){
+            Picasso.get().load(volume.volumeInfo.imageLinks.smallThumbnail).into(holder.binding.imgCover)
+        }else{
+            holder.binding.imgCover.setImageResource(R.drawable.baseline_broken_image)
+        }
         holder.binding.txtTitle.text = volume.volumeInfo.title
         holder.binding.txtAuthor.text = volume.volumeInfo.authors?.joinToString() ?: ""
         holder.binding.txtPages.text = volume.volumeInfo.pageCount?.toString() ?: "-"
